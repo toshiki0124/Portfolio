7897c48cd72954ea8e76c33ec76e56c7725694d3
中谷年希 <ts.hiki1212@gmail.com>
2021-05-13 20:14:29 +0900

    [add]トップページの紹介文

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-        
+    <div class="container">
+        <div class="introduction">
+
+            <h1 class="app-name">Tennis Matching</h1>
+            <h3 class="about-the-application">Abount the Applicaiton</h3>
+            <p class="description">テニスプレイヤー向けのマッチングアプリです。<br>一緒にプレイする仲間を探しましょう！</p>
+
+            <p class="guest-accont">
+                以下でログインしていただくと機能の確認が可能です。<br>
+                Email : test@gmail.com<br>
+                Password : testtest            
+            </p>
+
+        </div>      
+    </div>
 @endsection
